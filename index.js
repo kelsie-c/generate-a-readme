f8fa011a6d2f2d8395bfb090170f73d7639417e9
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-03-25 10:20:42 -0400

    added user prompts and readme outline

--- index.js
+++ index.js
@@ -1,13 +1,113 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a brief project description:',
+        },
+        {
+            type: 'input',
+            name: 'installInstruct',
+            message: 'Please enter any instructions your user may need to install your project:',
+        },
+        {
+            type: 'input',
+            name: 'usageInfo',
+            message: 'Please enter all relevant usage information:',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please enter your contribution guidelines:',
+        },
+        {
+            type: 'input',
+            name: 'testInstruct',
+            message: 'Please provide testing instructions:',
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'Which licenses were used for this project?',
+            choices: ['MIT', 'GPLv3', 'GPL', 'Creative Commons'],
+        },
+        {
+            type: 'input',
+            name: 'userName',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (answers) =>
+    `# ${answers.title}
+
+    ## Description
+
+    ${answers.description}
+
+    ## Table of Contents
+
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Testing Instructions](#testing)
+    - [License](#license)
+    - [Questions](#questions)
+
+    ## Installation
+
+    ${answers.installation}
+
+    ## Usage
+
+    ${answers.usageInfo}
+
+    ## Contribution
+
+    ${answers.contribution}
+
+    ## Testing Instructions
+
+    ${answers.testInstruct}
+
+    ## License
+
+    ${answers.licenses}
+
+    ## Questions?
+
+    [${answers.userName}](https://www.github.com/${answers.username})
+
+    Email: ${answers.email}
+    `
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then((answers) => writeFileAsync('README.md', writeToFile(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
